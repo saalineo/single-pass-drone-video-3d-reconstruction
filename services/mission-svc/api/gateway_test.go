e97aef725adb6a84c7ed050412693caf81efc2be
arif39x <aliarif1168@gmail.com>
2026-09-09 18:55:22 +0530

    control and foundation integration

--- services/mission-svc/api/gateway_test.go
+++ services/mission-svc/api/gateway_test.go
@@ -40,7 +40,7 @@ func TestGatewayRestEndpoints(t *testing.T) {
 	_ = httpLis.Close()
 
 	go func() {
-		_ = RunGateway(ctx, lis.Addr().String(), httpAddr, db.NewMissionStore(nil))
+		_ = RunGateway(ctx, lis.Addr().String(), httpAddr, db.NewMissionStore(nil), nil)
 	}()
 
 	time.Sleep(150 * time.Millisecond)
